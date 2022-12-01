9aebcc4f8e92422b096504835e56b6ec43c0aed7
GustavoFFLima <frazao39@gmail.com>
2022-12-01 11:34:56 -0300

    tela 1

--- src/Contador.js
+++ src/Contador.js
@@ -1,5 +1,22 @@
-export default function Contador() {
-    return <p> oi </p>
- }
+import styled from"styled-components";
 
+export default function Contador() {
+    return <ContadorStyle> 0/4 CONCLUÍDOS </ContadorStyle>
+ }
  
+ const ContadorStyle = styled.div`
+    width: 100%;
+    min-height: 50px;
+    background-color: #FFFFFF;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Recursive';
+    font-weight: 400;
+    font-size: 18px;
+    color: #333333;
+    padding: 10px;  
+`
--- src/Pergunta.js
+++ src/Pergunta.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Setaplay from './assets/img/seta_play.png'
+import styled from 'styled-components';
 
 const cards = [
 	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
@@ -13,14 +14,39 @@ const cards = [
 ]
 
 export default function Pergunta() {
-    return<div>
-        {cards.map(cards => 
-            <>
-                <p>cards.question</p> 
-                <button>
-                    <img src={Setaplay} />
-                </button>
+    return  <>
+                {cards.map(cards => 
+                    <PerguntaStyled>
+                        <p >{cards.question}</p> 
+                        <button >
+                            <img src={Setaplay} />
+                        </button>
+                    </PerguntaStyled>
+                )}
             </>
-        )}
-    </div>
  }
+
+const PerguntaStyled = styled.div`  
+    width: 300px;
+    height: 35px;
+    background-color: #FFFFFF;
+    margin: 12px;
+    padding: 15px;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    p {
+        font-family: 'Recursive';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+        color: #333333;
+    }
+    button {
+        border: none;
+        background-color: #FFFFFF;
+}
+`
